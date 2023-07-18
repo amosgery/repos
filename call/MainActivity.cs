09faaa879617370385fb827ac43c1ab6e347e962
Amos Gery <amos_gerry@hotmail.com>
2023-07-18 14:15:33 +0300

    call

--- call/MainActivity.cs
+++ call/MainActivity.cs
@@ -4,6 +4,9 @@ using Android.OS;
 using Android.Runtime;
 using Android.Widget;
 using Android;
+using AndroidX.Core.App;
+using System;
+using Android.Content.PM;
 
 namespace call
 {
@@ -11,11 +14,7 @@ namespace call
     public class MainActivity : Activity, Android.Views.View.IOnClickListener
 
     {
-
         Button btnDial;
-
-
-
         protected override void OnCreate(Bundle savedInstanceState)
 
         {
@@ -43,23 +42,40 @@ namespace call
         public void OnClick(Android.Views.View view)
 
         {
-
+            string phoneNumber = "+972549797156";
             if (btnDial == view)
-
             {
 
-                Intent intent = new Intent();
+                int permissionCheck = (int)PackageManager.CheckPermission(Manifest.Permission.CallPhone, this.PackageName);
 
-                intent.SetAction(Intent.ActionCall);
-
-                Android.Net.Uri data = Android.Net.Uri.Parse("tel:" + "52-11101111");
-
-                intent.SetData(data);
-
-                StartActivity(intent);
+                if (permissionCheck == 0)
+                {
+                    // The permission is already granted, so make the call
+                    makeCall(phoneNumber);
+                }
+                else
+                {
+                    // The permission is not granted, so request it
+                    ActivityCompat.RequestPermissions(this, new String[] { Manifest.Permission.CallPhone }, 1);
+                    permissionCheck = (int)PackageManager.CheckPermission(Manifest.Permission.CallPhone, this.PackageName);
+                    if (permissionCheck == 0)
+                    {
+                        // The permission is already granted, so make the call
+                        makeCall(phoneNumber);
+                    }
+                    else
+                        Toast.MakeText(this, "Permission Denied", ToastLength.Short).Show();
+                }
 
             }
 
+        }
+
+        public void makeCall(string phoneNumber)
+        {
+            Intent intent = new Intent(Intent.ActionCall);
+            intent.SetData(Android.Net.Uri.Parse("tel:" + phoneNumber));
+            StartActivity(intent);
         }
 
 
