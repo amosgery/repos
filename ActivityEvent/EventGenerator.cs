74bef67659c073366639a951b5a468b659f406d6
Amos Gery <amos_gerry@hotmail.com>
2023-11-26 18:44:24 +0200

    update

---
+++ ActivityEvent/EventGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Timers;
+
+namespace ActivityEvent
+{
+    public class EventGenerator
+    {
+        // Define an event
+        public event EventHandler DataUpdated;
+        Timer timer;
+        int seconds;
+        public EventGenerator()
+        {
+            timer = new Timer(1000);
+            timer.Elapsed += OnTimerElapsed;
+
+            // Start the timer
+            timer.Start();
+        }
+
+        private void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            seconds++;
+            OnDataUpdated();
+        }
+
+        // Method that triggers the event
+        private void OnDataUpdated()
+        {
+            CustomEventArgs eventArgs = new CustomEventArgs(seconds);
+            DataUpdated?.Invoke(this, eventArgs);
+        }
+
+    }
+
+}
---
+++ ActivityEvent/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+using Java.Util;
+using System;
+
+namespace ActivityEvent
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        private TextView textView;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            textView = FindViewById<TextView>(Resource.Id.textView);
+
+            // Create an instance of YourClass
+            var eventGenerator = new EventGenerator();
+
+            // Subscribe to the DataUpdated event
+            eventGenerator.DataUpdated += onDataUpdated;
+
+        }
+
+        // Event handler for DataUpdated event
+        private void onDataUpdated(object sender, EventArgs e)
+        {
+            CustomEventArgs ev = (CustomEventArgs)e;
+            // Update your activity UI here
+            textView.Text = "Seconds: "+ev.Value;
+        }
+    }
+}
