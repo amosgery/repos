06c4be83b3048cb1e6ea21856cb6cd7166925e77
Amos Gery <amos_gerry@hotmail.com>
2024-12-10 09:52:58 +0200

    add

---
+++ Map.net/MainActivity.cs
@@ -0,0 +1,54 @@
+using Android.Gms.Maps;
+using Android.Gms.Maps.Model;
+using Android.Media;
+using Android.Runtime;
+
+namespace Map.net
+{
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
+        public class MainActivity : Activity, IOnMapReadyCallback
+        {
+            private GoogleMap googleMap;
+
+            protected override void OnCreate(Bundle savedInstanceState)
+            {
+                base.OnCreate(savedInstanceState);
+                // Set our view from the "main" layout resource
+                SetContentView(Resource.Layout.activity_main);
+                MapFragment mapFrag = MapFragment.NewInstance();
+                FragmentManager.BeginTransaction()
+                                        .Add(Resource.Id.map, mapFrag, "map_fragment")
+                                        .Commit();
+                //var mapFragment = (MapFragment)FragmentManager.FindFragmentById(Resource.Id.map);
+                mapFrag.GetMapAsync(this);
+
+                // remainder of code omitted
+            }
+
+            public void OnMapReady(GoogleMap map)
+            {
+                googleMap = map;
+
+                // Set up a click listener for the map
+                googleMap.MapClick += GoogleMapOnMapClick;
+            }
+
+            private void GoogleMapOnMapClick(object sender, GoogleMap.MapClickEventArgs e)
+            {
+                LatLng clickedLatLng = e.Point;
+                double latitude = clickedLatLng.Latitude;
+                double longitude = clickedLatLng.Longitude;
+
+                // Now you have the latitude and longitude of the clicked location
+                Toast.MakeText(this, $"Clicked at: Lat {latitude}, Lng {longitude}", ToastLength.Short).Show();
+            }
+
+            public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+            {
+
+                base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            }
+
+        }
+    
+}
