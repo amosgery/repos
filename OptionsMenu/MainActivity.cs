d7623551ee31013c0ac53b82c110a542da8c824c
Amos Gery <amos_gerry@hotmail.com>
2025-03-30 16:02:17 +0300

    add

---
+++ OptionsMenu/MainActivity.cs
@@ -0,0 +1,43 @@
+
+using Android.Views;
+
+namespace OptionsMenu
+{
+
+
+    [Activity(Label = "Options Menu Example", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+        }
+
+        // Inflate the menu from the XML file
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.main_menu, menu);
+            return true; // Return true to display the menu
+        }
+
+        // Handle menu item clicks
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Resource.Id.action_settings:
+                    Toast.MakeText(this, "Settings Clicked", ToastLength.Short).Show();
+                    return true;
+
+                case Resource.Id.action_about:
+                    Toast.MakeText(this, "About Clicked", ToastLength.Short).Show();
+                    return true;
+
+                default:
+                    return base.OnOptionsItemSelected(item);
+            }
+        }
+    }
+
+}
